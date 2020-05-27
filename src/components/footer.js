b37eee8530a17816d33f176f32b5144bb6dd1e53
Jose Leiva <jdleiva@backcountry.com>
2020-05-26 19:46:35 -0600

    Add link to RSS

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,3 +1,4 @@
+import { Link } from "gatsby"
 import React from "react"
 
 import styles from "../styles/footer.module.css"
@@ -6,7 +7,12 @@ const Footer = () => (
   <footer>
     <small>
       Desarrollo y contenido por Jos&eacute; David Leiva{" "}
-      <span className={styles.date}>2012 - {new Date().getFullYear()}</span>
+      <span className={styles.date}>
+        2012 - {new Date().getFullYear()} /{" "}
+        <Link to="/rss.xml">
+          <strong>RSS</strong>
+        </Link>
+      </span>
     </small>
   </footer>
 )
