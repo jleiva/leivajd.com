d5fba0a254451d51b55b572e98b8ce9574f23f19
Jose Leiva <jdleiva@backcountry.com>
2020-07-11 23:22:38 -0600

    add author microformat

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -9,37 +9,44 @@ import styles from "../styles/blog-post.module.css"
 export default function BlogPost({ data, pageContext }) {
   const { next, previous } = pageContext
   const post = data.markdownRemark
-  const { title, tags, tweet } = post.frontmatter
+  const { title, tags, tweet, type } = post.frontmatter
 
   return (
     <Layout>
       <SEO description={post.excerpt} title={title} />
       <div className={`h-entry ${styles.postWrap}`}>
+        <span className={`p-category ${styles.category}`}>{type}</span>
         <h1 className={`p-name ${styles.postTitle}`}>{title}</h1>
         {/* ToDo: 
           1. Missing attr datetime
           2. Add itemprop to post
         */}
-        <div className={styles.metadata}>
-          <time className="dt-published">{post.frontmatter.date}</time>
-        </div>
         <div
           className={`e-content ${styles.post}`}
           dangerouslySetInnerHTML={{ __html: post.html }}
         />
 
         <div className={styles.syncWrap}>
+          <p>
+            Publicado el{" "}
+            <time className="dt-published">{post.frontmatter.date}</time>
+            {" "}por{" "}
+            <Link className="p-author h-card" to="/">
+              Jos&eacute; Leiva
+            </Link>
+          </p>
           <p className={`p-category ${styles.meta}`}>
             {tags.length ? `Tags ${tags.join(", ")}` : ""}
+
+            {tweet && (
+              <span className={styles.sync}>
+                Tambi&eacute;n en{" "}
+                <a href={tweet} className="u-syndication">
+                  Twitter
+                </a>
+              </span>
+            )}
           </p>
-          {tweet && (
-            <p className={styles.meta}>
-              Tambi&eacute;n en{" "}
-              <a href={tweet} className="u-syndication">
-                Twitter
-              </a>
-            </p>
-          )}
         </div>
 
         {/* Used to publish content to Twitter using https://brid.gy */}
@@ -79,6 +86,7 @@ export const query = graphql`
         title
         tags
         tweet
+        type
       }
       excerpt
     }
